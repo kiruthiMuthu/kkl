33859b0503d6f1e9362ecfc9b324ccedb3ec49b9
kiruthiMuthu <kiruthiIDOK@gmail.com>
2017-07-08 12:47:25 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,14 @@
+package even;
+import java.util.Scanner;
+public class even {
+	public static void main(String args[]) {
+		int sum=0;
+		for(int i=1;i<=50;i++)
+		{
+		if(i%2==0)
+		sum=sum+i;
+		}
+		System.out.println(sum);
+	}
+	
+}
